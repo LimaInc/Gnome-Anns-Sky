038a8da2896c237cd8d32721e208098a4e0f1bf8
DazKins <DazKins@users.noreply.github.com>
2018-01-19 17:59:22 +0000

    Added perlin noise sampling

--- Noise.cs
+++ Noise.cs
@@ -38,7 +38,7 @@ public class Noise
         return new int[] {x,y};
     }
 
-    public Vector2 getGrad(int x, int y)
+    private Vector2 getGrad(int x, int y)
     {
         int nx = intToNat(x);
         int ny = intToNat(y);
@@ -55,4 +55,57 @@ public class Noise
         randGrads.Add(z, v);
         return v;
     }
+
+    private int fastFloor(float x)
+	{
+		return x > 0 ? (int) x : (int) x - 1;
+	}
+
+	private float weigh(float x)
+	{
+		return 6 * x * x * x * x * x - 15 * x * x * x * x + 10 * x * x * x;
+	}
+
+	private float lerp(float w, float a, float b)
+	{
+		return a + w * (b - a);
+	}
+
+    public float sample(float x, float y)
+	{
+		int gx0 = fastFloor(x);
+		int gy0 = fastFloor(y);
+		int gx1 = gx0 + 1;
+		int gy1 = gy0 + 1;
+
+		float dx0 = x - gx0;
+		float dy0 = y - gy0;
+		float dx1 = x - gx1;
+		float dy1 = y - gy1;
+
+		Vector2 vals0 = getGrad(gx0, gy0);
+		Vector2 vals1 = getGrad(gx0, gy1);
+		Vector2 vals2 = getGrad(gx1, gy0);
+		Vector2 vals3 = getGrad(gx1, gy1);
+
+		float w0 = (new Vector2(dx0,dy0)).Dot(vals0);
+		float w1 = (new Vector2(dx0,dy1)).Dot(vals1);
+		float w2 = (new Vector2(dx1,dy0)).Dot(vals2);
+		float w3 = (new Vector2(dx1,dy1)).Dot(vals3);
+
+		float sx = weigh(dx0);
+		float sy = weigh(dy0);
+
+		float a = lerp(sy, w0, w1);
+		float b = lerp(sy, w2, w3);
+		float h = lerp(sx, a, b);
+
+        //Failsafe clipping
+		if (h < -1.0f)
+			h = -1.0f;
+		if (h > 1.0f)
+			h = 1.0f;
+
+		return h;
+	}
 }
