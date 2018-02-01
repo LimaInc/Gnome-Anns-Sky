555a1c6d233d0352c409f24c83fc02765f280efd
DazKins <DazKins@users.noreply.github.com>
2018-02-01 12:22:58 +0000

    Player can now place blocks

--- Scripts/Interaction.cs
+++ Scripts/Interaction.cs
@@ -26,14 +26,18 @@ public class Interaction : Camera
     }
 
     bool removeBlock = false;
+    bool placeBlock = false;
     public override void _Input(InputEvent e)
     {
         if(e is InputEventMouseButton)
         {
             InputEventMouseButton inputEvent = (InputEventMouseButton)e;
 
-            if(inputEvent.ButtonIndex == 1 && inputEvent.Pressed) //Only triggers on button down
+            if (inputEvent.ButtonIndex == 1 && inputEvent.Pressed) //Only triggers on button down
                 removeBlock = true;
+
+            if (inputEvent.ButtonIndex == 2 && inputEvent.Pressed)
+                placeBlock = true;
         }
     }
 
@@ -60,6 +64,28 @@ public class Interaction : Camera
                 IntVector3 blockPos = new IntVector3((int)Mathf.Round(pos.x / Chunk.BLOCK_SIZE), (int)Mathf.Round(pos.y / Chunk.BLOCK_SIZE), (int)Mathf.Round(pos.z / Chunk.BLOCK_SIZE));
 
                 terrain.SetBlock(blockPos, 0);
+            }
+        }
+        if (placeBlock)
+        {
+            placeBlock = false;
+
+            Vector3 from = this.ProjectRayOrigin(midScreenPoint);
+            Node[] exc = new Node[1];
+            exc[0] = GetNode("/root/Node/Player/physicsBody");
+            Dictionary<object, object> hitInfo = spaceState.IntersectRay(from, from + this.ProjectRayNormal(midScreenPoint) * rayLength, exc);
+
+            foreach(KeyValuePair<object, object> entry in hitInfo)
+            {
+                // do something with entry.Value or entry.Key
+            }
+
+            if(hitInfo.Count != 0) //Hit something
+            {
+                Vector3 pos = (Vector3)hitInfo["position"] + (Vector3)hitInfo["normal"] * 0.5f * Chunk.BLOCK_SIZE;
+                IntVector3 blockPos = new IntVector3((int)Mathf.Round(pos.x / Chunk.BLOCK_SIZE), (int)Mathf.Round(pos.y / Chunk.BLOCK_SIZE), (int)Mathf.Round(pos.z / Chunk.BLOCK_SIZE));
+
+                terrain.SetBlock(blockPos, 1);
             }
         }
     }
