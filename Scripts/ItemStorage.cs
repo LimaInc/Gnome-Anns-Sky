950cc7125fe7751c8813c464ffecd10e7c8f4d5a
DazKins <DazKins@users.noreply.github.com>
2018-02-04 22:53:27 +0000

    (Forgot to add files for previous commit .... again )

---
+++ Scripts/ItemStorage.cs
@@ -0,0 +1,14 @@
+using System;
+using Godot;
+
+public class ItemStorage
+{
+    private static Texture BLOCK_TEX = ResourceLoader.Load("res://Images/itemBlock.png") as Texture;
+    public static Item block = new Item(1, BLOCK_TEX, Item.Type.BLOCK);
+
+    private static Texture FOSSIL_TEX = ResourceLoader.Load("res://Images/itemFossil.png") as Texture;
+    public static Item fossil = new Item(2, FOSSIL_TEX, Item.Type.FOSSIL);
+
+    private static Texture CHOCOLATE_TEX = ResourceLoader.Load("res://Images/itemChocolate.png") as Texture;
+    public static Item chocoloate = new Item(3, CHOCOLATE_TEX, Item.Type.CONSUMABLE);
+}
