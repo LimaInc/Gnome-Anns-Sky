81a11aacc601bb82f2d92a171b0afffa92530e2c
DazKins <DazKins@users.noreply.github.com>
2018-02-05 14:26:56 +0000

    Added a better cursor

--- Scripts/Item.cs
+++ Scripts/Item.cs
@@ -10,15 +10,6 @@ public class Item
 
     public static Item[] items = new Item[256];
 
-    private static Texture BLOCK_TEX = ResourceLoader.Load("res://Images/itemBlock.png") as Texture;
-    public static Item block = new Item(1, BLOCK_TEX, Item.Type.BLOCK);
-
-    private static Texture FOSSIL_TEX = ResourceLoader.Load("res://Images/itemFossil.png") as Texture;
-    public static Item fossil = new Item(2, FOSSIL_TEX, Item.Type.FOSSIL);
-
-    private static Texture CHOCOLATE_TEX = ResourceLoader.Load("res://Images/itemChocolate.png") as Texture;
-    public static Item chocoloate = new Item(3, CHOCOLATE_TEX, Item.Type.CONSUMABLE);
-
     private byte id;
     private Texture texture;
     private Type type;
