f86682e3de0b9dfdcbfef1d2e0dd81ea900d9c2b
david <gamecake1@gmail.com>
2018-02-15 11:32:11 +0000

    Player now has a more flat starting zone, ready for the base to be built

--- Scripts/World/WorldGenerator.cs
+++ Scripts/World/WorldGenerator.cs
@@ -8,6 +8,11 @@ public class WorldGenerator
     private byte stoneId = Game.GetBlockId<Stone>();
     private byte grassId = Game.GetBlockId<RedRock>();
 
+    private static float SIGMOID_PARAM_A = -40.0f;
+    private static float SIGMOID_PARAM_B = 4.9f;
+
+    private static float STARTING_HEIGHT = 55.0f;
+
     public byte[,,] GetChunk(int x, int z, int sX, int sY, int sZ)
     {
         byte[,,] chunk = new byte[sX, sY, sZ];
@@ -18,13 +23,19 @@ public class WorldGenerator
             {
                 float xs = (x * sX + i) / 256.0f;
                 float zs = (z * sZ + k) / 256.0f;
-                int height = (int) (noise.sample(xs, zs) * 128) + 50;
+                float height = noise.sample(xs, zs) * 128.0f + 50.0f;
+
+                float centreDist = (float) Math.Sqrt(xs * xs + zs * zs);
+
+                float sigmoidSample = sigmoid(centreDist, SIGMOID_PARAM_A, SIGMOID_PARAM_B);
+
+                float weighted = sigmoidSample * STARTING_HEIGHT + (1 - sigmoidSample) * height;
 
                 for(int j = 0; j < sY; j++)
                 {
-                    if(j < height)
+                    if(j < weighted)
                         chunk[i,j,k] = stoneId;
-                    else if(j < height + 3) //3 layers of rock on top of stone
+                    else if(j < weighted + 3) //3 layers of rock on top of stone
                         chunk[i,j,k] = grassId;
                     else
                         chunk[i,j,k] = 0;
@@ -33,5 +44,10 @@ public class WorldGenerator
         }
 
         return chunk;
+    }
+
+    public float sigmoid(float x, float a, float b)
+    {
+        return (float) Math.Exp(a * x + b) / (1.0f + (float) Math.Exp(a * x + b));
     }
 }
