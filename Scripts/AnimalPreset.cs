45d327e529416045bfb22ee206aaf371a863faba
Samuel Silvester <samuel.silvester@hogpog.co.uk>
2018-02-15 04:21:47 +0000

    Breeding behaviour in entities without the breeding

--- Scripts/AnimalPreset.cs
+++ Scripts/AnimalPreset.cs
@@ -7,12 +7,16 @@ public class AnimalPreset
     public PackedScene scene;
     public AnimalBehaviourComponent.Diet diet;
     public int foodChainLevel;
+    public int breedability; //0 to 100
+    public string presetName;
 
-    public AnimalPreset(AnimalBehaviourComponent.Sex sex, PackedScene scene, AnimalBehaviourComponent.Diet diet, int foodChainLevel)
+    public AnimalPreset(AnimalBehaviourComponent.Sex sex, PackedScene scene, AnimalBehaviourComponent.Diet diet, int foodChainLevel, int breedability, string presetName)
     {
         this.sex = sex;
         this.scene = scene;
         this.diet = diet;
         this.foodChainLevel = foodChainLevel;
+        this.breedability = breedability;
+        this.presetName = presetName;
     }
 }
