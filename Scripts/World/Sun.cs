4cfb321bd0d293ec7fb90fe8e72b0b702607d9f3
JamesHinshelwood <jameshinshelwood1@gmail.com>
2018-03-02 16:44:01 +0000

    Removed unnecessary members from Sun

--- Scripts/World/Sun.cs
+++ Scripts/World/Sun.cs
@@ -3,16 +3,9 @@ using System;
 
 public class Sun : DirectionalLight
 {
-    WorldEnvironment worldEnvironment;
-    Godot.Environment environment;
-    ProceduralSky sky;
-
     public override void _Ready()
     {
-        worldEnvironment = GetNode(Game.WORLD_ENVIRO_PATH) as WorldEnvironment;
-        environment = worldEnvironment.GetEnvironment();
-
-        sky = environment.GetSky() as ProceduralSky;
+        ProceduralSky sky = ((GetNode(Game.WORLD_ENVIRO_PATH) as WorldEnvironment).GetEnvironment()).GetSky() as ProceduralSky;
 
         sky.SunLatitude = -this.RotationDegrees.x;
         sky.SunLongitude = 180 - this.RotationDegrees.y;
